3fe14856d1a598726c2172b500566c561b4aee07
Ruinous Potato <fusiek@gmail.com>
2017-10-10 09:47:28 +0200

    JQuery executed

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
+$(document).ready(function(){
+  $('#box1').css('color','#FE5A1D');
+})
+
+
+
+
+
+/*
 document.addEventListener("DOMContentLoaded",function(event){
   let elem = document.getElementById("box1");
   elem.style.color = '#FE5A1D'
 });
+*/
