ba654901349b785561ed8391e26f910d0cf2f72c
Ruinous Potato <fusiek@gmail.com>
2017-10-10 10:33:02 +0200

    Added relations

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-  $('#box1').css('color','#FE5A1D');
-})
+  // odniesienia do identyfikatorów
+  $("#box1").css('color','#FE5A1D');
+  $("#box2").css('color','#6082B6');
+  $("#box3").css('color','#A57C00');
+  // odniesienia do klas
+  $(".box").css('font-size','30px');
+});
 
 
 
@@ -11,4 +16,15 @@ document.addEventListener("DOMContentLoaded",function(event){
   let elem = document.getElementById("box1");
   elem.style.color = '#FE5A1D'
 });
+*/
+
+/*
+$('#box1').css('color','#FE5A1D');
+document.write('Hello world1');
+document.write('Hello world2');
+*/
+
+/*
+$("#tu").html("Dakka dakka");
+$(".tu2").html("More dakka");
 */
