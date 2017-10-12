3cbd3288617ef05e03d16a2931138bd3afb7888a
Ruinous Potato <fusiek@gmail.com>
2017-10-12 12:21:19 +0200

    End of initial training

--- main.js
+++ main.js
@@ -56,6 +56,25 @@ function listener_box() {
   });
 }
 
+function zmienMnie(){
+  wartosc = $('#wartosc').val();
+  wartosc2 = zamien_na_hex(wartosc);
+  $('#wartosc2').val(wartosc2);
+}
+
+function zamien_na_hex(wartosc){
+  switch(wartosc){
+    case '10': text='A'; break;
+    case '11': text='B'; break;
+    case '12': text='C'; break;
+    case '13': text='D'; break;
+    case '14': text='E'; break;
+    case '15': text='F'; break;
+    default: text = wartosc;
+  }
+  return text;
+}
+
 /*
 document.addEventListener("DOMContentLoaded",function(event){
   let elem = document.getElementById("box1");
